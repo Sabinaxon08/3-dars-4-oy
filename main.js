f6d53c6228523695bbf1881f58df8e8ab96d7aa2
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-07-03 12:11:06 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let random =  Math.ceil(Math.random() * 10)
+let kopaytiruv = random * 2
+let boluv =  random / 2
+
+console.log(random)
+console.log (kopaytiruv)
+console.log ( boluv)
+
+
+
+
+
+    
